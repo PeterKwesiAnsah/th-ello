294365ddb7948c4c667d73046f1c674e1638c2e9
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2024-06-07 16:59:38 +0000

    feat: mui theme

---
+++ src/lib/mui.tsx
@@ -0,0 +1,38 @@
+import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+	typography: {
+		fontFamily: 'Mulish, Arial',
+	},
+	components: {
+		MuiCssBaseline: {
+			styleOverrides: `
+        @font-face {
+          font-family: 'Mulish';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+        }
+      `,
+		},
+	},
+});
+
+export const AppThemeProvider = ({
+	children,
+}: {
+	children: React.ReactNode;
+}) => {
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			{children}
+		</ThemeProvider>
+	);
+};
+
+// // ...
+// return (
+
+// );
