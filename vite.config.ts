aaf8878baf3c4d214ab629ff45afcaed0bc84844
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2024-06-07 17:27:30 +0000

    chore: setup path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			//module loader replaces @ with the absolute path of folder src
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+});
